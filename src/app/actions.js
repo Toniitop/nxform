9ea2ba3e77db988b81128c958694e8727d2e8b55
Antonio Ortiz <tonitoortiz17@gmail.com>
2023-12-13 18:44:50 +0100

    cambios de insercion de campos nuevos

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,14 +3,30 @@
 import fs from 'node:fs'
 
 export async function handle(formData) {
-    const nombre = formData.get('nombre')
-    const apellidos = formData.get('apellidos')
-    const avatar = formData.get('avatar')
-    
+    const nombre = formData.get('nombre');
+    const apellidos = formData.get('apellidos');
+    const avatar = formData.get('avatar');
+    const passwd = formData.get('passwd');
+    const opcionSeleccionada = formData.get('opciones');
+    const opcionSeleccionadaC = formData.get('opcionesC');
+    const opcionSeleccionadaSel = formData.get('opcionesSel');
+    const mensaje = formData.get('mensaje');
+    const fechaHora = formData.get('fechaHora');
+    const mes = formData.get('mes');
+    const semana = formData.get('semana');
+    const rango = formData.get('rango');
+    const dni = formData.get('dni');
+    const busqueda = formData.get('busqueda');
+    const telefono = formData.get('telefono');
+    const email = formData.get('email');
+    const url = formData.get('url');
+
     const buffer = await avatar.arrayBuffer()
     const bytes = new Uint8Array(buffer)
 
-    // console.log(nombre, apellidos, avatar)
+    console.log(nombre, apellidos, avatar, passwd, opcionSeleccionada, opcionSeleccionadaC,
+        opcionSeleccionadaSel,`mensaje de textarea: ${mensaje}`, fechaHora, mes, semana,
+        rango, dni, busqueda, telefono, email, url);
 
     await new Promise((resolve) => setTimeout( resolve, 1000))
 
--- src/app/formulario.jsx
+++ src/app/formulario.jsx
@@ -13,12 +13,104 @@ export function Formulario() {
 
     return (
         <form action={wrapper}>
-            <input type="text" required name="nombre" placeholder="Introduce tu nombre" />
-            <input type="text" required name="apellidos" placeholder="Introduce tus apellidos" />
-            <label htmlFor="avatar">
-                Selecciona un avatar para enviar al servidor
+            <label>
+                Nombre:
+                <input type="text" required name="nombre" placeholder="Introduce tu nombre" />
             </label>
-            <input type="file" required name="avatar" accept="image/*" />
+            <label>
+                Apellidos:
+                <input type="text" required name="apellidos" placeholder="Introduce tus apellidos" />
+            </label>
+            <label>
+                Selecciona un avatar para enviar al servidor:
+                <input type="file" required name="avatar" accept="image/*" />
+            </label>
+            <label>
+                Contraseña:
+                <input type="password" name="passwd" placeholder="Introduce tu contraseña" />
+            </label>
+            <br />
+            <label>
+                Marca una opción:
+                <br />
+                    <label>
+                        Opción 1: <input type="radio" name='opciones'/>
+                    </label>
+                    <label>
+                        Opción 2: <input type="radio" name='opciones'/>
+                    </label>
+                    <label>
+                        Opción 3: <input type="radio" name='opciones'/>
+                    </label>
+            </label>
+            <br />
+            <label>
+                Marca una opción (checkbox):
+                <br />
+                    <label>
+                        Opción 1: <input type="checkbox" name='opcionesC'/>
+                    </label>
+                    <label>
+                        Opción 2: <input type="checkbox" name='opcionesC'/>
+                    </label>
+                    <label>
+                        Opción 3: <input type="checkbox" name='opcionesC'/>
+                    </label>
+            </label>
+            <br />
+            <label>
+                Elije una opción:
+                <select name='opcionesSel'>
+                    <option value="op1">Opción 1</option>
+                    <option value="op2">Opción 2</option>
+                    <option value="op3">Opción 3</option>
+                </select>
+            </label>
+            <br />
+            <br />
+            <label>
+                Escriba lo que quiera:
+                <br />
+                <textarea name="mensaje" placeholder='Escribe..' cols="50" rows="20"></textarea>
+            </label>
+            <br />
+            <br />
+            <label>
+                Fecha y hora:
+                <input type="datetime-local" name='fechaHora' placeholder="Fecha y hora" />
+            </label>
+            <label>
+                Mes:
+                <input type="month" name='mes' placeholder="Mes" />
+            </label>
+            <label>
+                Semana:
+                <input type="week" name='semana' placeholder="Semana" />
+            </label>
+            <label>
+                Selecciona un valor:
+                <input type="range" name="rango" min="0" max="100" step="1"></input>
+            </label>
+            <label>
+                DNI:
+                <input type="number" name='dni' placeholder="DNI (sin letra)" />
+            </label>
+            <label>
+                Búsqueda:
+                <input type="search" name='busqueda' placeholder="Busca" />
+            </label>
+            <label>
+                Teléfono:
+                <input type="tel" name='telefono' placeholder="Nº móvil" />
+            </label>
+            <label>
+                Email:
+                <input type="email" name='email' placeholder="Enter your email" />
+            </label>
+            <label>
+                URL:
+                <input type="url" name='url' placeholder="URL" />
+            </label>
             <SubmitButton />
         </form>
     )
